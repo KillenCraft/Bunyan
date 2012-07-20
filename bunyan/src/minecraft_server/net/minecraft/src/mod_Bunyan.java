24dceb3cb3d8b731fd6024306ae20879ecc0edef
Scott Killen <scott@scottkillen.com>
2012-07-20 08:25:35 -0400

    Split files to fix server side compile errors in rendering code

---
+++ bunyan/src/minecraft_server/net/minecraft/src/mod_Bunyan.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) Scott Killen, 2012
+ * 
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license
+ * located in /MMPL-1.0.txt
+ */
+
+package net.minecraft.src;
+
+import java.util.Random;
+
+import net.minecraft.src.forge.NetworkMod;
+import bunyan.Bunyan;
+
+/**
+ * This class appears only to communicate with the mod loader.
+ * Everything is delegated to {@link Bunyan.Bunyan}
+ * 
+ */
+public class mod_Bunyan extends NetworkMod {
+
+	@Override
+	public boolean clientSideRequired() {
+		return Bunyan.clientSideRequired();
+	}
+
+	@Override
+	public void generateSurface(World world, Random random, int chunkX,
+			int chunkZ)
+	{
+		Bunyan.onGenerateSurface(world, random, chunkX, chunkZ);
+	}
+
+	@Override
+	public String getName() {
+		return Bunyan.getName();
+	}
+
+	@Override
+	public String getVersion() {
+		return Bunyan.getVersion();
+	}
+
+	@Override
+	public void load() {
+		Bunyan.onLoad(this);
+	}
+
+	@Override
+	public void modsLoaded() {
+		Bunyan.onModsLoaded();
+	}
+
+}
